193c81eec648c91cd629b9155076447ff926149c
wangguangwu <wangguangwu@pukanghealth.com>
2023-01-04 11:07:19 +0800

    SpringBoot starter

---
+++ spring-boot-integration-01-starter/src/main/java/com/wangguangwu/starter/SpringBootIntegration01StarterApplication.java
@@ -0,0 +1,15 @@
+package com.wangguangwu.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class SpringBootIntegration01StarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntegration01StarterApplication.class, args);
+    }
+}
